8d046bf1acd0b8aa7e98d52e878fcdeea396b74e
DarkestGhost <alihp6120@gmail.com>
2026-06-08 18:57:03 +0330

    backend ready for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,11 @@
 import jsonServer from "json-server";
-import auth from "json-server-auth";
 
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 
-server.db = router.db;
-
 server.use(jsonServer.defaults());
-server.use(auth);
 server.use(router);
 
-// مهم: پورت داینامیک
 const PORT = import.meta.env.PORT || 3000;
 
-server.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
-});
+server.listen(PORT);
